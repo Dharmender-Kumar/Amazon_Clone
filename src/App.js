30927a42f834af62275819a2a8d7f8144940e47a
Dharmender-Kumar <82511641+Dharmender-Kumar@users.noreply.github.com>
2022-07-22 22:15:33 +0530

    add to cart with sum count and basic home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Checkout from "./Checkout";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //BEM convention
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/checkout" element={<Checkout />}></Route>
+        </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDnm876vts5VDGn8FYBW038ZxkMWmzEs-Q",
+  authDomain: "clone-8ed5d.firebaseapp.com",
+  projectId: "clone-8ed5d",
+  storageBucket: "clone-8ed5d.appspot.com",
+  messagingSenderId: "988341228124",
+  appId: "1:988341228124:web:cf30327d807259fd940c99",
+  measurementId: "G-RES395GJTD",
+};
